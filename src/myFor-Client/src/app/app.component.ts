19581292dccc115d94694cca40ca79723ce52fe7
myfor <mrmyfor@outlook.com>
2020-06-23 16:25:48 +0800

    add global file in client

--- src/myFor-Client/src/app/app.component.ts
+++ src/myFor-Client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { GlobalService } from './global';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass', './app.component-layout.sass']
 })
-export class AppComponent {
-  title = 'myfor';
+export class AppComponent implements OnInit {
+  constructor(
+    private global: GlobalService,
+    @Inject(DOCUMENT) private doc: any
+  ) {}
+
+  ngOnInit(): void {
+    this.global.title.subscribe(title => {
+      this.doc.title = title;
+    });
+  }
 }
---
+++ src/myFor-Client/src/app/global.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { enc } from 'crypto-js';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GlobalService {
+
+    // tslint:disable-next-line: variable-name
+    private static _title: BehaviorSubject<string> = new BehaviorSubject<string>('myFor');
+
+    get title(): BehaviorSubject<string> {
+        return GlobalService._title;
+    }
+
+    setTitle(title: string) {
+        GlobalService._title.next(title.trim() ? title : '');
+    }
+}
+
+const IDENTITY_KEY = 'NO9A[';
+export interface IdentityInfo {
+    nickName: string;
+    account: string;
+    avatar: string;
+}
+/**
+ * 当前登录用户
+ */
+export class Identity {
+
+    /**
+     * 当前登录用户信息，
+     */
+    static get IdentityInfo(): IdentityInfo {
+        const VALUE = localStorage.getItem(IDENTITY_KEY);
+        if (VALUE === null) {
+            return null;
+        }
+        const SOURCE_VALUE: string = enc.Base64.parse(VALUE).toString(enc.Utf8);
+        return JSON.parse(SOURCE_VALUE);
+    }
+    static set IdentityInfo(info: IdentityInfo) {
+        if (!info) {
+            localStorage.removeItem(IDENTITY_KEY);
+            return;
+        }
+        let value = JSON.stringify(info);
+        value = enc.Utf8.parse(value);
+        value = enc.Base64.stringify(value);
+        localStorage.setItem(IDENTITY_KEY, value);
+    }
+    static clean() {
+        localStorage.removeItem(IDENTITY_KEY);
+    }
+}
