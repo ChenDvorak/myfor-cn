990e7acdfc505594ffe7fbf4d357f69a9931eb2f
myfor <mrmyfor@outlook.com>
2020-06-28 10:02:46 +0800

    add login and register components

--- src/myFor-Client/src/app/routers/login-out/login-out.module.ts
+++ src/myFor-Client/src/app/routers/login-out/login-out.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 
 import { LoginOutRoutingModule } from './login-out-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     SharedModule,
     LoginOutRoutingModule
