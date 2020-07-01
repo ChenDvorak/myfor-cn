bc1ee0ceaa524b42161d84d661adaf1a92e364f0
myfor <mrmyfor@outlook.com>
2020-07-01 16:31:48 +0800

    user page blog list

--- src/myFor-Client/src/app/routers/users/users-routing.module.ts
+++ src/myFor-Client/src/app/routers/users/users-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DetailComponent,
     children: [
-
+      { path: '', component: BlogListComponent, pathMatch: 'full' }
     ]
   },
   { path: '**', redirectTo: '/pages/404' }
--- src/myFor-Client/src/app/routers/users/users.module.ts
+++ src/myFor-Client/src/app/routers/users/users.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 
 import { LoginOutModule } from '../../components/login-out/login-out.module';
+import { BlogsModule } from '../../components/blogs/blogs.module';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { DetailComponent } from './detail/detail.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 
 @NgModule({
-  declarations: [DetailComponent],
+  declarations: [DetailComponent, BlogListComponent],
   imports: [
     SharedModule,
     UsersRoutingModule,
-    LoginOutModule
+    LoginOutModule,
+    BlogsModule
   ]
 })
 export class UsersModule { }
